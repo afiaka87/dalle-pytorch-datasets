7b8ece22568c031144a3e3b4022b9b09cb038793
Sam Sepiol <>
2021-03-24 18:52:35 -0500

    (add) STL-10 images

--- STL-10/download.sh
+++ STL-10/download.sh
@@ -3,3 +3,11 @@
 # Enable strict mode.
 set -euo pipefail
 IFS=$'\n\t'
+
+mkdir output;
+
+aria2c https://academictorrents.com/download/a799a2845ac29a66c07cf74e2a2838b6c5698a6a.torrent; # STL-10
+
+
+# extract STL-10
+tar -xf stl10_binary.tar.gz --directory=output;
