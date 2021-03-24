aa2c698bb83338d106b4157aecba2bd2b265cffa
Sam Sepiol <>
2021-03-24 18:15:40 -0500

    Set correct tar dir name for visualgenome.

--- captioned/VisualGenome/download.sh
+++ captioned/VisualGenome/download.sh
@@ -5,9 +5,17 @@ set -euo pipefail
 IFS=$'\n\t'
 
 mkdir output;
-mkdir download;
+
+TAR_DIR="VG_100K_2";
 
 aria2c --split=4 --lowest-speed-limit=10K --parameterized-uri https://academictorrents.com/download/1bfe6871046860a2ff8c0cc1414318beb35dc916.torrent;
 
-unzip -q ./download/images.zip
-unzip -q ./download/images2.zip
+cd $TAR_DIR;
+
+unzip -q images.zip;
+rm -rf images.zip;
+
+unzip -q images2.zip;
+rm -rf images2.zip;
+
+cd ../;
