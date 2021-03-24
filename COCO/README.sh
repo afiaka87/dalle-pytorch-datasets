9b832e61b59667382036b01159eb266fdf61acef
Sam Sepiol <>
2021-03-24 18:39:31 -0500

    (add) COCO_2017

---
+++ COCO/README.sh
@@ -0,0 +1,2 @@
+# COCO 2017 (~200,000 images) 
+Training set downscaled to 256px. Captions in the folder.
--- COCO/download.sh
+++ COCO/download.sh
@@ -3,3 +3,6 @@
 # Enable strict mode.
 set -euo pipefail
 IFS=$'\n\t'
+
+wget https://www.dropbox.com/s/dtjjz9cpenmpowr/train2017.zip;
+unzip -q train2017.zip;
