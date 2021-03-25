91728facb2b9a4d346e30fceacd48878fd763146
Sam Sepiol <>
2021-03-24 19:08:23 -0500

    (add) SVHN images

---
+++ SVHN/README.md
@@ -0,0 +1,8 @@
+# The Street View House Numbers (SVHN) Dataset
+
+
+Size: 2.64 GiB
+
+Images: 600K
+
+Captions: Need to be generated.
--- SVHN/download.sh
+++ SVHN/download.sh
@@ -3,3 +3,15 @@
 # Enable strict mode.
 set -euo pipefail
 IFS=$'\n\t'
+
+mkdir output;
+
+aria2c https://academictorrents.com/download/6f4caf3c24803d114c3cae3ab9cb946cd23c7213.torrent; # SVHN
+
+# extracting SVHN
+for i in extra.tar.gz test.tar.gz train.tar.gz
+do 
+	echo "Untarring: $i";
+	tar -xf $i --directory=output;
+done
+
