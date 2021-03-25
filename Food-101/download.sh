60d2c08b506d63a1df4fd8e8831444dc39d876ce
Sam Sepiol <>
2021-03-24 19:03:25 -0500

    (add) food-101

--- Food-101/download.sh
+++ Food-101/download.sh
@@ -3,3 +3,10 @@
 # Enable strict mode.
 set -euo pipefail
 IFS=$'\n\t'
+
+aria2c https://academictorrents.com/download/470791483f8441764d3b01dbc4d22b3aa58ef46f.torrent; # food-101
+
+mkdir output;
+
+tar -xf food-101.tgz --directory=output;
+rm food-101.tgz;
